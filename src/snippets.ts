cb9cadadbcccabc47039fa5cdc61f383ae0c05f4
bruno <reisalvesb@gmail.com>
2023-10-18 16:38:13 -0300

    Fix snippets

--- src/snippets.ts
+++ src/snippets.ts
@@ -1,15 +1,4 @@
 import * as vscode from 'vscode';
 
-class Snippet {
-    newOperationSnippet() {
-        return new vscode.SnippetString(",\t{\n\t\t\"operation\": \"${1|shift,default,remove,sort,cardinality,modify-default-beta,modify-overwrite-beta|}\",\n\t\t\"spec\": {}\n\t}\n");
-    }
-    
-    autoCompleteOperationSnippet() {
-        return new vscode.SnippetString(": \"${1|shift,default,remove,sort,cardinality,modify-default-beta,modify-overwrite-beta|}\",");
-    }
-    
-}
-
-export const newOperationSnippet = new Snippet().newOperationSnippet();
-export const autoCompleteSnippet = new Snippet().autoCompleteOperationSnippet();
+export const newOperationSnippet = new vscode.SnippetString(",\t{\n\t\t\"operation\": \"${1|shift,default,remove,sort,cardinality,modify-default-beta,modify-overwrite-beta|}\",\n\t\t\"spec\": {}\n\t}\n");
+export const autoCompleteSnippet = new vscode.SnippetString(": \"${1|shift,default,remove,sort,cardinality,modify-default-beta,modify-overwrite-beta|}\",");
